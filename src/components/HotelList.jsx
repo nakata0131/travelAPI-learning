a5d7b2c7e4c937ac87d88f98be950e41e775d047
Nakata Daiki <24aw0131@jec.ac.jp>
2025-02-12 18:33:48 +0900

    mariginを880pxから80pxに修正

--- src/components/HotelList.jsx
+++ src/components/HotelList.jsx
@@ -11,7 +11,7 @@ export default function HotelList({ hotels }) {
         const hotelInfo = hotelItem.hotel[0].hotelBasicInfo;
 
         return (
-          <li key={index} style={{ marginBottom: "880px", listStyle: "none" }}>
+          <li key={index} style={{ marginBottom: "80px", listStyle: "none" }}>
             <h3>{hotelInfo.hotelName}</h3>
             <img src={hotelInfo.hotelImageUrl} alt={hotelInfo.hotelName} width="200" />
             <p>{hotelInfo.address1} {hotelInfo.address2}</p>
